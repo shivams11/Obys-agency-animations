e66cc0f26ff4a4a632b2c6e3959912aa1f15256f
Shivam Yogendra Saner <shivamsanery2003@gmail.com>
2024-05-16 19:16:22 +0530

    hi22

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function locomotiveAnimation() {
   gsap.registerPlugin(ScrollTrigger);
 
-  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
@@ -16,7 +15,7 @@ function locomotiveAnimation() {
       return arguments.length
         ? locoScroll.scrollTo(value, 0, 0)
         : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    }, 
     getBoundingClientRect() {
       return {
         top: 0,
@@ -25,7 +24,7 @@ function locomotiveAnimation() {
         height: window.innerHeight,
       };
     },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    
     pinType: document.querySelector("#main").style.transform
       ? "transform"
       : "fixed",
